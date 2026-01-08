88ee43fd15712607d5efea0564b186b1608f96c3
Soniccooles <andrey.sonic@mail.ru>
2026-01-09 00:01:44 +0500

    макет для занятия и адаптер к нему

--- app/src/main/java/com/itandrew/androidlab2/adapter/LessonAdapter.kt
+++ app/src/main/java/com/itandrew/androidlab2/adapter/LessonAdapter.kt
@@ -1,5 +1,43 @@
 package com.itandrew.androidlab2.adapter
 
-class LessonAdapter {
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.itandrew.androidlab2.data.Lesson
+import com.itandrew.androidlab2.databinding.ItemLessonBinding
+
+class LessonAdapter(private val lessons: ArrayList<Lesson>) : RecyclerView.Adapter<LessonAdapter.LessonViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): LessonViewHolder {
+        val binding = ItemLessonBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return LessonViewHolder(binding)
+    }
+
+
+    override fun onBindViewHolder(
+        holder: LessonViewHolder,
+        position: Int
+    ) {
+        val lesson = lessons[position]
+        holder.bind(lesson)
+    }
+
+    override fun getItemCount(): Int {
+        return lessons.size
+    }
+
+    class LessonViewHolder(private val binding: ItemLessonBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(lesson: Lesson) {
+            binding.lessonTime.text = "${lesson.startTime} - ${lesson.endTime}"
+            binding.lessonRoom.text = lesson.room
+            binding.lessonSubject.text = lesson.subject
+            binding.lessonTeacher.text = lesson.teacher
+        }
+    }
 }
